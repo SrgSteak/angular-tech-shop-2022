5e6bf8352358c86e69646a232691b892d131f8b7
Robin Schmidt <schmidt91robin@icloud.com>
2022-09-30 09:22:37 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,35 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { StartpageComponent } from './startpage/startpage.component';
+import { AboutpageComponent } from './aboutpage/aboutpage.component';
+import { SearchpageComponent } from './searchpage/searchpage.component';
+import { SearchformComponent } from './searchpage/searchform/searchform.component';
+import { ResultlistComponent } from './searchpage/resultlist/resultlist.component';
+import { ResultitemComponent } from './searchpage/resultitem/resultitem.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([
+    {
+      path: '',
+      pathMatch: 'full',
+      component: StartpageComponent
+    },
+    {
+      path: 'about',
+      component: AboutpageComponent
+    },
+    {
+      path: 'search',
+      component: SearchpageComponent
+    },
+    {
+      path: '*',
+      redirectTo: ''
+    }
+  ]) ],
+  declarations: [ AppComponent, HelloComponent, StartpageComponent, AboutpageComponent, SearchpageComponent, SearchformComponent, ResultlistComponent, ResultitemComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
